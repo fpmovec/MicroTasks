53686dd0b89aac035709d9124dd63be0a1d94c8c
fpmovec <10.maks.10K@gmail.com>
2022-12-22 22:52:24 +0300

    Added composite

---
+++ Composite/Composite.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite_Iterator_Visitor.Composite
+{
+    public abstract class Component
+    {
+        protected string? Name { get; set; }
+        public Component(string? name) => Name = name;
+        public abstract void Add(Component component);
+        public abstract void Remove(Component component);
+        public abstract List<Component> GetComponents();
+        public abstract void PrintNodes();
+    }
+
+    class Directory : Component
+    {
+        List<Component> components = new();
+        public Directory(string name) : base(name) { }
+
+        public override void Add(Component component) 
+            => components.Add(component);
+        public override void Remove(Component component)
+            => components.Remove(component);
+        public override List<Component> GetComponents()
+            => components;
+        public override void PrintNodes()
+        {
+            Console.WriteLine("Node: " + Name);
+            foreach (var component in components)
+                component.PrintNodes();
+        }
+    }
+
+    class File : Component
+    {
+        public File(string name) : base(name) { }
+        public override void Add(Component component)
+        {
+            throw new InvalidOperationException();
+        }
+        public override void Remove(Component component)
+        {
+            throw new InvalidOperationException();
+        }
+        public override List<Component> GetComponents()
+        {
+            throw new InvalidOperationException();
+        }
+        public override void PrintNodes()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Composite_Iterator_Visitor.Composite;
+using Directory = Composite_Iterator_Visitor.Composite.Directory;
+using File = Composite_Iterator_Visitor.Composite.File;
+try
+{
+    Component fileSystem = new Directory("File System");
+
+    Component diskD = new Directory("D://");
+
+    Component secretFolder = new Directory("Secret Folder");
+    Component passwordsTxt = new File("passwords.txt");
+    secretFolder.Add(passwordsTxt);
+    diskD.Add(secretFolder);
+
+    Component photosFolder = new Directory("Folder with files");
+    Component iPhotoPng = new File("AntonSemenchenko.png");
+    Component youPhotoPng = new File("patterns.png");
+    photosFolder.Add(iPhotoPng);
+    photosFolder.Add(youPhotoPng);
+    diskD.Add(photosFolder);
+
+    fileSystem.Add(diskD);
+    fileSystem.PrintNodes();
+    youPhotoPng.Add(iPhotoPng);
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine("Cannot add node in file!");
+}
