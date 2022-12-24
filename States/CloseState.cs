efb8e6f81db74a5291c6933326775cc2563c68a9
fpmovec <10.maks.10K@gmail.com>
2022-12-24 13:40:23 +0300

    State pattern added

---
+++ States/CloseState.cs
@@ -0,0 +1,22 @@
+﻿using State.DB;
+using State.Interfaces;
+
+namespace State.States
+{
+    public class CloseState : IDBState
+    {
+        public void Open(DBConnection db)
+        {
+            Console.WriteLine("Opening is succesfully\tclose --> open");
+            db.state = new OpenState();
+        }
+        public void Process(DBConnection db)
+        {
+            Console.WriteLine("Processing cannot be performed");
+        }
+        public void Close(DBConnection db)
+        {
+            Console.WriteLine("Connection is already closed");
+        }
+    }
+}
---
+++ States/OpenState.cs
@@ -0,0 +1,23 @@
+﻿using State.DB;
+using State.Interfaces;
+
+namespace State.States
+{
+    public class OpenState : IDBState
+    {
+        public void Open(DBConnection db)
+        {
+            Console.WriteLine("Connection is already opened");
+        }
+        public void Process(DBConnection db)
+        {
+            Console.WriteLine("Processing is succesfully\topen --> process");
+            db.state = new ProcessState();
+        }
+        public void Close(DBConnection db)
+        {
+            Console.WriteLine("Closing is succesfully\topen --> close");
+            db.state = new CloseState();
+        }
+    }
+}
